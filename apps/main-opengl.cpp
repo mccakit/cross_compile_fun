5acf680bbeef329d8c3778c9e83f5ad1cf72590b
mccakit <117523238+mccakit@users.noreply.github.com>
2025-06-04 11:33:35 +0300

    wasm fixed

---
+++ apps/main-opengl.cpp
@@ -0,0 +1,193 @@
+#include "imgui.h"
+#include "imgui_impl_sdl3.h"
+#include "imgui_impl_opengl3.h"
+#include <stdio.h>
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+#include <SDL3/SDL_opengl.h>
+#include <SDL3_mixer/SDL_mixer.h>
+
+#define _CRT_SECURE_NO_WARNINGS
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+// Simple helper function to load an image into a OpenGL texture with common settings
+bool LoadTextureFromMemory(const void* data, size_t data_size, GLuint* out_texture, int* out_width, int* out_height)
+{
+    // Load from file
+    int image_width = 0;
+    int image_height = 0;
+    unsigned char* image_data = stbi_load_from_memory((const unsigned char*)data, (int)data_size, &image_width, &image_height, NULL, 4);
+    if (image_data == NULL)
+        return false;
+
+    // Create a OpenGL texture identifier
+    GLuint image_texture;
+    glGenTextures(1, &image_texture);
+    glBindTexture(GL_TEXTURE_2D, image_texture);
+
+    // Setup filtering parameters for display
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    // Upload pixels into texture
+    glPixelStorei(GL_UNPACK_ROW_LENGTH, 0);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image_width, image_height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image_data);
+    stbi_image_free(image_data);
+
+    *out_texture = image_texture;
+    *out_width = image_width;
+    *out_height = image_height;
+
+    return true;
+}
+
+// Open and read a file, then forward to LoadTextureFromMemory()
+bool LoadTextureFromFile(const char* file_name, GLuint* out_texture, int* out_width, int* out_height)
+{
+    FILE* f = fopen(file_name, "rb");
+    if (f == NULL)
+        return false;
+    fseek(f, 0, SEEK_END);
+    size_t file_size = (size_t)ftell(f);
+    if (file_size == -1)
+        return false;
+    fseek(f, 0, SEEK_SET);
+    void* file_data = IM_ALLOC(file_size);
+    fread(file_data, 1, file_size, f);
+    fclose(f);
+    bool ret = LoadTextureFromMemory(file_data, file_size, out_texture, out_width, out_height);
+    IM_FREE(file_data);
+    return ret;
+}
+
+
+struct app_state
+{
+    SDL_Window* window;
+    SDL_WindowFlags window_flags;
+    SDL_GLContext gl_context;
+    ImGuiIO ioObject;
+    ImGuiIO &io = ioObject;
+    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+    SDL_AppResult app_quit = SDL_APP_CONTINUE;
+    //audio
+    SDL_AudioDeviceID audioDevice {};
+    Mix_Music * music {};
+    //img
+    int my_image_width = 0;
+    int my_image_height = 0;
+    GLuint my_image_texture = 0;
+};
+app_state state{};
+SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[])
+{
+    SDL_Init(SDL_INIT_VIDEO | SDL_INIT_GAMEPAD);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+
+    // Create window with graphics context
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+    SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+    state.window_flags = SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_HIDDEN | SDL_WINDOW_HIGH_PIXEL_DENSITY;
+    state.window = SDL_CreateWindow("Dear ImGui SDL3+OpenGL3 example", 1280, 720, state.window_flags);
+    state.gl_context = SDL_GL_CreateContext(state.window);
+    SDL_GL_MakeCurrent(state.window, state.gl_context);
+    SDL_GL_SetSwapInterval(1); // Enable vsync
+    SDL_SetWindowPosition(state.window, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED);
+    SDL_ShowWindow(state.window);
+    IMGUI_CHECKVERSION();
+    // Setup Dear ImGui context
+    ImGui::CreateContext();
+    state.io = ImGui::GetIO();
+    state.io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+    state.io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;
+    // Setup Dear ImGui style
+    ImGui::StyleColorsDark();
+    // Setup Platform/Renderer backends
+    ImGui_ImplSDL3_InitForOpenGL(state.window, state.gl_context);
+    ImGui_ImplOpenGL3_Init("#version 300 es");
+
+    bool ret = LoadTextureFromFile("gs_tiger.png", &state.my_image_texture, &state.my_image_width, &state.my_image_height);
+    IM_ASSERT(ret);
+
+    state.audioDevice = SDL_OpenAudioDevice(SDL_AUDIO_DEVICE_DEFAULT_PLAYBACK, nullptr);
+    Mix_OpenAudio(state.audioDevice, nullptr);
+    state.music = Mix_LoadMUS("the_entertainer.ogg");
+    Mix_PlayMusic(state.music, -1);
+
+    return SDL_APP_CONTINUE;
+}
+SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event* event)
+{
+    // Poll and handle events (inputs, window resize, etc.)
+    ImGui_ImplSDL3_ProcessEvent(event);
+    if (event->type == SDL_EVENT_QUIT)
+    {
+        state.app_quit = SDL_APP_SUCCESS;
+    }
+    if (event->type == SDL_EVENT_WINDOW_CLOSE_REQUESTED &&
+        event->window.windowID == SDL_GetWindowID(state.window))  // Use the event parameter
+    {
+        state.app_quit = SDL_APP_SUCCESS;
+    }
+    return SDL_APP_CONTINUE;
+}
+SDL_AppResult SDL_AppIterate(void *appstate)
+{
+    if (state.app_quit != SDL_APP_CONTINUE)
+        return state.app_quit;
+
+    if (SDL_GetWindowFlags(state.window) & SDL_WINDOW_MINIMIZED)
+    {
+        SDL_Delay(10);
+        return SDL_APP_CONTINUE;
+    }
+    // Start the Dear ImGui frame
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplSDL3_NewFrame();
+    ImGui::NewFrame();
+    ImGui::Begin("OpenGL Texture Test");
+    ImGui::Text("FPS: %.1f", ImGui::GetIO().Framerate);
+    ImGui::Image((ImTextureID)(intptr_t)state.my_image_texture, ImVec2(480, 480));
+    static bool music_playing {true};
+    if (ImGui::Button(music_playing ? "Pause" : "Resume"))
+    {
+        if (music_playing)
+        {
+            Mix_PauseMusic();
+        }
+        else
+        {
+            Mix_ResumeMusic();
+        }
+        music_playing = !music_playing;
+    }
+    ImGui::End();
+    ImGui::Render();
+    glViewport(0, 0, (int)state.io.DisplaySize.x, (int)state.io.DisplaySize.y);
+    glClearColor(state.clear_color.x * state.clear_color.w, state.clear_color.y * state.clear_color.w, state.clear_color.z * state.clear_color.w, state.clear_color.w);
+    glClear(GL_COLOR_BUFFER_BIT);
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+    SDL_GL_SwapWindow(state.window);
+    return SDL_APP_CONTINUE;
+}
+void SDL_AppQuit(void* appstate, SDL_AppResult result)
+{
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplSDL3_Shutdown();
+
+    ImGui::DestroyContext();
+    SDL_GL_DestroyContext(state.gl_context);
+
+    Mix_FadeOutMusic(1000);
+    Mix_FreeMusic(state.music);
+    Mix_CloseAudio();
+    SDL_CloseAudioDevice(state.audioDevice);
+
+    SDL_DestroyWindow(state.window);
+    SDL_Quit();
+}
