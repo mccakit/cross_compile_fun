9e17b6a405bd37223e931244139131971613254d
mccakit <117523238+mccakit@users.noreply.github.com>
2025-06-04 23:44:51 +0300

    OpenGL 4.6

--- apps/main-opengl.cpp
+++ apps/main-opengl.cpp
@@ -99,12 +99,12 @@ SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[])
         SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
         SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
     #else
-        // GL 3.0 + GLSL 130
-        const char* glsl_version = "#version 130";
+        //OpenGL 4.6
+        const char* glsl_version = "#version 460";
         SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
         SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-        SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-        SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 6);
     #endif
     // Create window with graphics context
     SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
