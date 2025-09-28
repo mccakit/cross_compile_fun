ebe0e5e39d0dd29e9ae19f16d6932ab58f9263ce
mccakit <mccakit09@gmail.com>
2025-09-28 20:01:52 +0300

    wasm lto

--- toolchains/emcc-wasm32.cmake
+++ toolchains/emcc-wasm32.cmake
@@ -1,3 +1,3 @@
 include(/home/mccakit/dev/emscripten/cmake/Modules/Platform/Emscripten.cmake)
-add_compile_options(-w -sASSERTIONS -sALLOW_MEMORY_GROWTH)
-add_link_options(-w -sASSERTIONS -sALLOW_MEMORY_GROWTH)
+add_compile_options(-w -sALLOW_MEMORY_GROWTH -sINITIAL_MEMORY=256MB -flto)
+add_link_options(-w -sALLOW_MEMORY_GROWTH -sINITIAL_MEMORY=256MB -flto)
--- toolchains/emcc-wasm64.cmake
+++ toolchains/emcc-wasm64.cmake
@@ -1,3 +1,3 @@
 include(/home/mccakit/dev/emscripten/cmake/Modules/Platform/Emscripten.cmake)
-add_compile_options(-w -sMEMORY64=1 -sUSE_PTHREADS=1 -sASSERTIONS -sALLOW_MEMORY_GROWTH -sMAXIMUM_MEMORY=12GB)
-add_link_options(-w -sMEMORY64=1 -sUSE_PTHREADS=1 -sASSERTIONS -sALLOW_MEMORY_GROWTH -sMAXIMUM_MEMORY=12GB)
+add_compile_options(-w -sMEMORY64=1 -sUSE_PTHREADS=1 -sALLOW_MEMORY_GROWTH -sINITIAL_MEMORY=256MB -sMAXIMUM_MEMORY=12GB -flto)
+add_link_options(-w -sMEMORY64=1 -sUSE_PTHREADS=1 -sALLOW_MEMORY_GROWTH -sINITIAL_MEMORY=256MB -sMAXIMUM_MEMORY=12GB -flto)
